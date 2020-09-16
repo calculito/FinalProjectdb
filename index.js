fe82e9b70a0c7193059fcc5d0fb0848c24c21c84
calculito <ion.oaie@gmail.com>
2020-09-16 07:48:43 +0200

    stars and stripes

--- index.js
+++ index.js
@@ -142,7 +142,7 @@ app.get("/userhomeworksSYES/:id", function (req, res) {
   }
   pool
     .query(
-      "select name, h2.link , hf.finished, linkhwfinished  from users u inner join homework_finished hf on hf.user_id = u.id inner join homeworks h2 on h2.id = hf.homeworks_id where finished='yes' and  u.name=$1;",
+      "select name, h2.link, h2.optional, hf.finished, linkhwfinished  from users u inner join homework_finished hf on hf.user_id = u.id inner join homeworks h2 on h2.id = hf.homeworks_id where finished='yes' and  u.name=$1;",
       [username]
     )
     .then((result) => res.json(result.rows))
@@ -156,7 +156,7 @@ app.get("/userhomeworksSNO/:id", function (req, res) {
   }
   pool
     .query(
-      "select * from (select name, h2.link, (select hf.finished from   homework_finished hf inner join homeworks h on hf.homeworks_id = h2.id inner join users u2 on u.id = hf.user_id where finished = 'yes' and u.id = u2.id and h2.id = h.id)  from homeworks h2 inner join class c2 on c2.id = h2.class_id inner join users u on c2.id = u.class_id where u.name=$1) foo where foo.finished IS DISTINCT from 'yes'",
+      "select * from (select name, h2.id, h2.optional, h2.link, (select hf.finished from   homework_finished hf inner join homeworks h on hf.homeworks_id = h2.id inner join users u2 on u.id = hf.user_id where finished = 'yes' and u.id = u2.id and h2.id = h.id)  from homeworks h2 inner join class c2 on c2.id = h2.class_id inner join users u on c2.id = u.class_id where u.name=$1) foo where foo.finished IS DISTINCT from 'yes'",
       [username]
     )
     .then((result) => res.json(result.rows))
@@ -170,20 +170,21 @@ app.get("/userhomeworksALL/:id", function (req, res) {
   }
   pool
     .query(
-      "select h.id, h.link, u2.name, u2.user_role, (select hf.finished from   homework_finished hf inner join homeworks h2 on hf.homeworks_id = h2.id inner join users u on u.id = hf.user_id where u.id = u2.id and h2.id = h.id ), (select hf.linkhwfinished from   homework_finished hf inner join homeworks h2 on hf.homeworks_id = h2.id inner join users u on u.id = hf.user_id where u.id = u2.id and h2.id = h.id ) from   homeworks h inner join class c2 on c2.id = h.class_id inner join users u2 on c2.id = u2.class_id where c2.id = $1 and u2.user_role = 'Student' ORDER BY h.id DESC;",
+      "select h.id,h.optional, h.link, u2.name, u2.user_role, (select hf.finished from   homework_finished hf inner join homeworks h2 on hf.homeworks_id = h2.id inner join users u on u.id = hf.user_id where u.id = u2.id and h2.id = h.id ), (select hf.linkhwfinished from   homework_finished hf inner join homeworks h2 on hf.homeworks_id = h2.id inner join users u on u.id = hf.user_id where u.id = u2.id and h2.id = h.id ) from   homeworks h inner join class c2 on c2.id = h.class_id inner join users u2 on c2.id = u2.class_id where c2.id = $1 and u2.user_role = 'Student' ORDER BY h.id DESC;",
       [userclass]
     )
     .then((result) => res.json(result.rows))
     .catch((e) => console.error(e));
 });
 ///////////  CHANGE STATUS HOMEWORK   /////////////////
-app.put("/homeworkfinished/:homeworkId", function (req, res) {
+app.post("/homeworkfinished/:homeworkId", function (req, res) {
   let homeworkId = req.params.homeworkId;
-  const newEmail = req.body.email;
+  const userId = req.body.userId;
   pool
-    .query("UPDATE homework_finished SET finished = 'yes' WHERE id=$1", [
-      homeworkId,
-    ])
+    .query(
+      "insert into homework_finished (homeworks_id, user_id, finished) values ($1, $2, 'yes')",
+      [homeworkId, userId]
+    )
     .then(() => res.status(200).send("homework updated"))
     .catch((error) => {
       console.log(error);
@@ -194,13 +195,30 @@ app.put("/homeworkfinished/:homeworkId", function (req, res) {
 app.put("/homeworkfinishedlink/:homeworkId", function (req, res) {
   let homeworkId = req.params.homeworkId;
   const linkToHomework = req.body.link;
+  const userId = req.body.userId;
   console.log(linkToHomework);
   pool
-    .query("UPDATE homework_finished SET linkhwfinished = $2 WHERE id=$1", [
-      homeworkId,
-      linkToHomework,
-    ])
+    .query(
+      "UPDATE homework_finished SET linkhwfinished = $2 WHERE homeworks_id=$1 AND user_id=$3",
+      [homeworkId, linkToHomework, userId]
+    )
     .then(() => res.status(200).send("link to homework updated"))
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send("something went wrong :( ...");
+    });
+});
+///////////  CHANGE HOMEWORK TO OPTIONAL /////////////////
+app.put("/homeworkoptional/:homeworkId", function (req, res) {
+  let homeworkId = req.params.homeworkId;
+  const homeworkOptional = req.body.optional;
+
+  pool
+    .query("UPDATE homeworks SET optional = $2 WHERE id=$1", [
+      homeworkId,
+      homeworkOptional,
+    ])
+    .then(() => res.status(200).send("homework optional updated"))
     .catch((error) => {
       console.log(error);
       res.status(500).send("something went wrong :( ...");
@@ -274,6 +292,19 @@ app.put("/personallinkstars/:linkId", function (req, res) {
 
   pool
     .query("UPDATE perslinks SET stars = $2 WHERE id=$1", [linkId, numberStars])
+    .then(() => res.status(200).send("Stars updated"))
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send("something went wrong :( ...");
+    });
+});
+///////////  CHANGE STARS GENERAL LINKS     /////////////////
+app.put("/generallinkstars/:linkId", function (req, res) {
+  let linkId = req.params.linkId;
+  const numberStars = req.body.link;
+
+  pool
+    .query("UPDATE links SET stars = $2 WHERE id=$1", [linkId, numberStars])
     .then(() => res.status(200).send("Stars updated"))
     .catch((error) => {
       console.log(error);
